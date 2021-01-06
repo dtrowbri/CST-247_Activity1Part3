7d7e27ceb2c60f081956adcead9c1df5d8aacf88
Donald_Trowbridge <dtrowbridg@my.gcu.edu>
2021-01-06 00:41:22 -0700

    Add project files.

---
+++ Activity1Part3/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Activity1Part3.Models;
+using Activity1Part3.Services.Business;
+
+namespace Activity1Part3.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserModel model)
+        {
+            if(new SecurityService().Authenticate(model))
+            {
+                return View("LoginPassed");
+            } else
+            {
+                return View("LoginFailed");
+            }
+        }
+    }
+}
---
+++ Activity1Part3/Services/Data/SecurityDAO.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+using Activity1Part3.Models;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Activity1Part3.Services.Data
+{
+    public class SecurityDAO
+    {
+        string connectionStr = ConfigurationManager.ConnectionStrings["DonaldLocal"].ConnectionString;
+        string ValidateLoginQuery = @"if exists(select * from Test.dbo.Users where username = @name and password = @password collate SQL_Latin1_General_CP1_CS_AS)
+                                        Begin
+                                            select 'True'
+                                        End
+                                        Else
+                                        Begin
+                                            select 'False'
+                                        End";
+        public bool FindByUser(UserModel user)
+        {
+            bool isLoginValid = false;
+            using(SqlConnection conn = new SqlConnection(connectionStr))
+            {
+                using(SqlCommand cmd = new SqlCommand(ValidateLoginQuery, conn))
+                {
+                    cmd.Parameters.AddWithValue("@name", user.Username);
+                    cmd.Parameters.AddWithValue("@password", user.Password);
+                    
+                    try
+                    {
+                        conn.Open();
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        reader.Read();
+                        if(reader[0].ToString() == "True")
+                        {
+                            isLoginValid = true;
+                        } 
+                    } catch(SqlException ex)
+                    {
+                        Debug.WriteLine(ex.Message);
+                    } catch(Exception ex)
+                    {
+                        Debug.WriteLine(ex.Message);
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
+                    return isLoginValid;
+                }
+            }
+        }
+    }
+}
