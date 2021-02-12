e4d3bf28851794f134a08ce5a82613decda9e53b
Donald_Trowbridge <dtrowbridg@my.gcu.edu>
2021-02-11 23:43:27 -0700

    commit

--- Activity1Part3/Controllers/LoginController.cs
+++ Activity1Part3/Controllers/LoginController.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 using Activity1Part3.Models;
 using Activity1Part3.Services.Business;
+using NLog;
+using Activity1Part3.Services.Utility;
 
 namespace Activity1Part3.Controllers
 {
     public class LoginController : Controller
     {
+
+        //private static Logger logger = LogManager.GetLogger("myAppLoggerRules");
         // GET: Login
         [HttpGet]
         public ActionResult Index()
@@ -20,19 +25,31 @@ namespace Activity1Part3.Controllers
         [HttpPost]
         public ActionResult Login(UserModel model)
         {
-            SecurityService service = new SecurityService();
-            bool authenticated = service.Authenticate(model);
+            MyLogger.getInstance().Info("Entering LoginController.Login()");
+            MyLogger.getInstance().Info("Parameters are: " + new JavaScriptSerializer().Serialize(model));
+            try
+            {
+                SecurityService service = new SecurityService();
+                bool authenticated = service.Authenticate(model);
 
 
-            if (!ModelState.IsValid)
+                if (!ModelState.IsValid)
+                {
+                    return View("Login");
+                }
+                if (authenticated)
+                {
+                    MyLogger.getInstance().Info("Exit LoginController.Login() with login passing");
+                    return View("LoginPassed");
+                }
+                else
+                {
+                    MyLogger.getInstance().Info("Exit LoginController.Login() with login failing");
+                    return View("LoginFailed");
+                }
+            }catch(Exception e)
             {
-                return View("Login");
-            }
-            if(authenticated)
-            {
-                return View("LoginPassed");
-            } else
-            {
+                MyLogger.getInstance().Error("Exception LoginController.Login()" + e.Message);
                 return View("LoginFailed");
             }
         }
---
+++ Activity1Part3/Services/Utility/MyLogger.cs
@@ -0,0 +1,60 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Activity1Part3.Services.Utility
+{
+    public class MyLogger : ILogger
+    {
+
+        private static MyLogger instance;
+        private static Logger logger;
+
+        private MyLogger()
+        {
+
+        }
+
+        public static MyLogger getInstance()
+        {
+            if(instance == null)
+            {
+                instance = new MyLogger();
+            }
+            return instance;
+        }
+
+        private Logger GetLogger(string loggerName)
+        {
+            if(MyLogger.logger == null)
+            {
+                MyLogger.logger = LogManager.GetLogger(loggerName);
+            }
+            return MyLogger.logger;
+        }
+
+
+        public void Debug(string message)
+        {
+            GetLogger("myAppLoggerRules").Debug(message);
+        }
+
+        public void Error(string message)
+        {
+            GetLogger("myAppLoggerRules").Error(message);
+        }
+
+        public void Info(string message)
+        {
+            GetLogger("myAppLoggerRules").Info(message);
+        }
+
+        public void Warning(string message)
+        {
+            GetLogger("myAppLoggerRules").Warn(message);
+        }
+    }
+}
